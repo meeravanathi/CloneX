a7d94e91bf4e68ee6d37f768cee2c3e3ce55bc74
Meera vanathi <119102584+meeravanathi@users.noreply.github.com>
2024-09-10 14:34:57 +0530

    Add files via upload

---
+++ client/src/pages/Explore/Explore.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ExploreTweets from "../../components/ExploreTweets/ExploreTweets";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
+
+import { useSelector } from "react-redux";
+import Signin from "../Signin/Signin";
+
+const Explore = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <>
+    {!currentUser ? (
+      <Signin />
+    ) : (
+    <div className="grid grid-cols-1 md:grid-cols-4">
+    <div className="px-6">
+      <LeftSidebar />
+    </div>
+    <div className="col-span-2 border-x-2 border-zinc-900 px-6">
+      <ExploreTweets />
+    </div>
+    <div className="px-6">
+      <RightSidebar />
+    </div>
+  </div>
+   )}
+   </>
+ );
+};
+
+export default Explore
